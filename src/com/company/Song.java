77d9fee0ba6508099195333bcc4c4c54088d21f0
UntilThenOcean <juuurihh@gmail.com>
2022-07-04 22:09:06 +0800

    Song application

---
+++ src/com/company/Song.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Song {
+
+    String title;
+    String artist;
+    String rating;
+    String bpm;
+
+    Song(String t, String a, String r, String b) {
+        title = t;
+        artist = a;
+        rating = r;
+        bpm = b;
+    }
+
+    public String getTitle() {
+        return getTitle();
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getBpm() {
+        return bpm;
+    }
+
+    public String toString() {
+        return title;
+    }
+}
